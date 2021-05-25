dc60cff2d7af0e4ce512a32d277c5ba1be4f3a47
František Mäsiar <gaxiwAWCj2TWNva>
2021-05-25 15:47:52 +0200

    added

--- src/main/java/sk/fri/uniza/App.java
+++ src/main/java/sk/fri/uniza/App.java
@@ -1,10 +1,37 @@
 package sk.fri.uniza;
 
+import retrofit2.Call;
+import retrofit2.Response;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Hello IoT!
  */
 public class App {
     public static void main(String[] args) {
+        IotNode iotNode = new IotNode();
+        // Vytvorenie požiadavky na získanie údajov o aktuálnom počasí z
+        // meteo stanice s ID: station_1
+        Call<Map<String, String>> currentWeather =
+                iotNode.getWeatherStationService()
+                        .getCurrentWeatherAsMap("station_1");
+
+        try {
+            // Odoslanie požiadavky na server pomocou REST rozhranie
+            Response<Map<String, String>> response = currentWeather.execute();
+
+            if (response.isSuccessful()) { // Dotaz na server bol neúspešný
+                //Získanie údajov vo forme Mapy stringov
+                Map<String, String> body = response.body();
+                System.out.println(body);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
